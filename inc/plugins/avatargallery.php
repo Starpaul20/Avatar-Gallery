f1fc881e1f6e7c9fe3014611a31df5090ba0a8ad
PaulBender <paulbender@live.com>
2015-04-08 11:58:18 -0400

    Bump to version v1.1.1

--- inc/plugins/avatargallery.php
+++ inc/plugins/avatargallery.php
@@ -45,7 +45,7 @@ function avatargallery_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.1",
+		"version"			=> "1.1.1",
 		"codename"			=> "avatargallery",
 		"compatibility"		=> "18*"
 	);
