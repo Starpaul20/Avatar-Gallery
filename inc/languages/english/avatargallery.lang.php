bb8c2f10b83e7015fffd2b7cc4d8f2fcf4308521
PaulBender <paulbender@live.com>
2017-11-19 14:19:18 -0500

    Updated error message

--- inc/languages/english/avatargallery.lang.php
+++ inc/languages/english/avatargallery.lang.php
@@ -8,7 +8,7 @@ $l['avatar_gallery'] = "Avatar Gallery";
 $l['avatargallery_info_name'] = "Avatar Gallery";
 $l['avatargallery_info_desc'] = "Adds an avatar gallery to your board.";
 
-$l['error_noavatar'] = "You did not choose an avatar. Please go back and do so now. If you don't want an avatar, select the \"No avatar\" option.";
+$l['error_noavatar'] = "You did not choose an avatar. Please go back and do so now.";
 $l['default_gallery'] = "Default Gallery";
 $l['avatars_in_gallery'] = "Avatars in {1}";
 
