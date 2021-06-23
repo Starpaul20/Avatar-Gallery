f1be4bff333769210a85adbc1cfda958172064e9
Paul Bender <paulbender@live.com>
2021-06-23 13:04:11 -0400

    Added check to see if THIS_SCRIPT is defined

--- inc/plugins/avatargallery.php
+++ inc/plugins/avatargallery.php
@@ -11,14 +11,17 @@ if(!defined("IN_MYBB"))
 }
 
 // Neat trick for caching our custom template(s)
-if(THIS_SCRIPT == 'usercp.php')
+if(defined('THIS_SCRIPT'))
 {
-	global $templatelist;
-	if(isset($templatelist))
+	if(THIS_SCRIPT == 'usercp.php')
 	{
-		$templatelist .= ',';
+		global $templatelist;
+		if(isset($templatelist))
+		{
+			$templatelist .= ',';
+		}
+		$templatelist .= 'usercp_avatar_gallery,usercp_avatar_gallery_avatar,usercp_avatar_gallery_option';
 	}
-	$templatelist .= 'usercp_avatar_gallery,usercp_avatar_gallery_avatar,usercp_avatar_gallery_option';
 }
 
 // Tell MyBB when to run the hooks
